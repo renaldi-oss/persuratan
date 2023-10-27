26cf67d5564f0332300e70c407e50a870f724093
renaldi-oss <reynaldifp2000@gmail.com>
2023-10-27 12:46:23 +0700

    final sebelum menggunakan livewire

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.login');
+    return view('guest.Login');
 });
 
 Route::get('/dashboard', function() {
