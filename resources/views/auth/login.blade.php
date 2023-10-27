290c645388c5e504754508da16a8dda797288303
renaldi-oss <reynaldifp2000@gmail.com>
2023-10-27 14:17:29 +0700

    tambah package laravel-debugbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,17 +10,17 @@
           <a href="#" class="h1"><b>TKI</b></a>
         </div>
         <div class="card-body">
-          <form id="loginForm">
-            <div class="input-group mb-3">
-              <input type="text" name="username" class="form-control" placeholder="Username" >
+          <form id="formLogin" action="{{ route('dashboard') }}" method="GET">
+            <div class="form-group input-group mb-3">
+              <input type="text" name="username" class="form-control" placeholder="Usermane">
               <div class="input-group-append">
                 <div class="input-group-text">
-                  <span class="fas fa-user"></span>
+                  <span class="fas fa-envelope"></span>
                 </div>
               </div>
             </div>
-            <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="Password">
+            <div class="form-group input-group mb-3">
+              <input type="password" name="password" class="form-control" placeholder="Password">
               <div class="input-group-append">
                 <div class="input-group-text">
                   <span class="fas fa-lock"></span>
@@ -28,16 +28,8 @@
               </div>
             </div>
             <div class="row">
-              <div class="col-8">
-                <div class="icheck-primary">
-                  <input type="checkbox" id="remember">
-                  <label for="remember">
-                    Remember Me
-                  </label>
-                </div>
-              </div>
               <!-- /.col -->
-              <div class="col-4">
+              <div class="col-12">
                 <button type="submit" class="btn btn-primary btn-block">Sign In</button>
               </div>
               <!-- /.col -->
@@ -54,45 +46,34 @@
 
 @section('script')
 
-<script src="{{ asset('/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+<script src="{{ asset('./plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('./plugins/jquery-validation/additional-methods.min.js') }}"></script>
 {{-- script jquery untuk validator input data --}}
 <script>
  $(function () {
-  $.validator.setDefaults({
-    submitHandler: function () {
-      alert( "Form successful submitted!" );
-    }
-  });
-  $('#loginForm').validate({
+  $('#quickForm').validate({
     rules: {
-      email: {
+      username: {
         required: true,
-        email: true,
       },
       password: {
         required: true,
-        minlength: 5
-      },
-      terms: {
-        required: true
-      },
+        minlength: 1
+      }
     },
     messages: {
-      email: {
-        required: "Please enter a email address",
-        email: "Please enter a valid email address"
+      username: {
+        required: "Please enter a valid username",
       },
       password: {
         required: "Please provide a password",
         minlength: "Your password must be at least 5 characters long"
-      },
-      terms: "Please accept our terms"
+      }
     },
     errorElement: 'span',
     errorPlacement: function (error, element) {
       error.addClass('invalid-feedback');
-      element.closest('.input-group').append(error);
+      element.closest('.form-group').append(error);
     },
     highlight: function (element, errorClass, validClass) {
       $(element).addClass('is-invalid');
