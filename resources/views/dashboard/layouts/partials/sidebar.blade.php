2f4d60347072727787bcb73625b0b1894240c4c9
Asrobika <asrobika01@gmail.com>
2023-11-01 10:01:51 +0700

    nata sidebar dikit..

--- resources/views/dashboard/layouts/partials/sidebar.blade.php
+++ resources/views/dashboard/layouts/partials/sidebar.blade.php
@@ -1,4 +1,5 @@
 <!-- Main Sidebar Container -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
@@ -29,19 +30,6 @@
         </li>
     </ul>
     </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-            <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-            </button>
-        </div>
-        </div>
-    </div>
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -69,7 +57,7 @@
 
         {{-- DIVIDER --}}
         <hr class="my-12 mx-1 bg-secondary" />
-        
+
         <li class="nav-item">
             <a href="#" class="nav-link">
             <i class="nav-icon fas fa-comment-exclamation"></i>
@@ -88,7 +76,7 @@
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-building-columns"></i>
+                <i class="nav-icon fas fa-solid fa-money-check-dollar" style="color: #c2c7d0;"></i>
             <p>
                 Administration & Finance
                 <i class="right fas fa-angle-left"></i>
@@ -105,7 +93,7 @@
                     </p>
                     </a>
                 </li>
-                @endhasrole     
+                @endhasrole
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
