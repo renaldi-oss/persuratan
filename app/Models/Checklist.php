0812fe538ebab48f8e37f867088d510c4e65b2ea
sunu <katsuragane@gmail.com>
2023-12-15 19:43:46 +0700

    Add Checklist relationship to WorkOrder model

--- app/Models/Checklist.php
+++ app/Models/Checklist.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Checklist extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function workOrder()
+    {
+        return $this->belongsTo(WorkOrder::class);
+    }
 }
